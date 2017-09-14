4bbc3ecfa4f7319245a48455e446dcdf7c0af3d6
LiMao <limao@iot.chinamobile.com>
2017-09-14 11:30:55 +0800

    update

---
+++ aotu_poweroff/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace aotu_poweroff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(99, 45);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(44, 21);
+            this.tb1.TabIndex = 0;
+            this.tb1.Text = "18";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "设置";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "自动关机时间：";
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Location = new System.Drawing.Point(12, 93);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(131, 12);
+            this.lb2.TabIndex = 3;
+            this.lb2.Text = "时间已设定为：18时2分";
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(170, 45);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(44, 21);
+            this.tb2.TabIndex = 4;
+            this.tb2.Text = "2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "时";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "分";
+            // 
+            // lb4
+            // 
+            this.lb4.AutoSize = true;
+            this.lb4.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb4.Location = new System.Drawing.Point(12, 147);
+            this.lb4.Name = "lb4";
+            this.lb4.Size = new System.Drawing.Size(186, 21);
+            this.lb4.TabIndex = 7;
+            this.lb4.Text = "距离自动关机还有";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(28, 210);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "自动启动";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.chk_changed);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "正在运行";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.dclick);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(295, 45);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(20, 20);
+            this.webBrowser1.TabIndex = 9;
+            this.webBrowser1.Url = new System.Uri("http://cc00559.csec.corp/em/sj/auto_poweroff.php?version=1.0", System.UriKind.Absolute);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "关于";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 235);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lb4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "自动关机小玩意";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_size_changed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ aotu_poweroff/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace aotu_poweroff
+{
+    public partial class Form1 : Form
+    {
+
+        public string cmd(string dosCommand)
+        {
+            return cmd(dosCommand, 30000);
+        }
+        /// <summary>
+        /// 执行DOS命令，返回DOS命令的输出
+        /// </summary>
+        /// <param name="dosCommand">dos命令</param>
+        /// <param name="milliseconds">等待命令执行的时间（单位：毫秒），
+        /// 如果设定为0，则无限等待</param>
+        /// <returns>返回DOS命令的输出</returns>
+        public static string cmd(string command, int seconds)
+        {
+            string output = ""; //输出字符串
+            if (command != null && !command.Equals(""))
+            {
+                Process process = new Process();//创建进程对象
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";//设定需要执行的命令
+                startInfo.Arguments = "/C " + command;//“/C”表示执行完命令后马上退出
+                startInfo.UseShellExecute = false;//不使用系统外壳程序启动
+                startInfo.RedirectStandardInput = false;//不重定向输入
+                startInfo.RedirectStandardOutput = true; //重定向输出
+                startInfo.CreateNoWindow = true;//不创建窗口
+                process.StartInfo = startInfo;
+                try
+                {
+                    if (process.Start())//开始进程
+                    {
+                        if (seconds == 0)
+                        {
+                            process.WaitForExit();//这里无限等待进程结束
+                        }
+                        else
+                        {
+                            process.WaitForExit(seconds); //等待进程结束，等待时间为指定的毫秒
+                        }
+                        output = process.StandardOutput.ReadToEnd();//读取进程的输出
+                    }
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    if (process != null)
+                        process.Close();
+                }
+            }
+            return output;
+        }
+
+
+        public Form1(string ss)
+        {
+            InitializeComponent(); sss = ss;
+        }
+            int set_hour=18;
+            int set_min=2;
+            string sss;
+        int sign15=0,sign10=0,sign5=0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            int hour = dt.Hour;
+            int min = dt.Minute;
+           
+            int shengxia = (set_hour - hour) * 60 + set_min - min;
+
+            lb4.Text = "距离自动关机还有" + shengxia + "分钟";
+            notifyIcon1.Text = "距离自动关机还有" + shengxia + "分钟";
+
+            if (shengxia < 16&&sign15==0) {
+                notifyIcon1.Visible = false;  //托盘图标隐藏
+                this.WindowState = FormWindowState.Normal;
+                this.Show();
+                this.ShowInTaskbar = true;
+                sign15 = 1;
+            }
+            else if (shengxia < 11&&sign10==0) {
+                notifyIcon1.Visible = false;  //托盘图标隐藏
+                this.WindowState = FormWindowState.Normal;
+                this.Show();
+                this.ShowInTaskbar = true;
+                sign10 = 1;
+            }
+            else if (shengxia < 6&&sign5==0)
+            {
+                 notifyIcon1.Visible = false;  //托盘图标隐藏
+                this.WindowState = FormWindowState.Normal;
+                this.Show();
+                this.ShowInTaskbar = true;
+                sign5 = 1;
+              
+            }
+            else if(shengxia ==1){             
+                notifyIcon1.Visible = false;  //托盘图标隐藏
+                this.WindowState = FormWindowState.Normal;
+                this.Show();
+                this.ShowInTaskbar = true;}
+
+            else if (shengxia == 0) {// cmd("shutdown -s /t 300");
+             cmd("shutdown -s /t 10");
+             timer1.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (sss == "s")
+            {
+                this.Hide(); 
+                this.ShowInTaskbar = false;  //不显示在系统任务栏
+                notifyIcon1.Visible = true;  //托盘图标可见
+                this.notifyIcon1.ShowBalloonTip(500, "提示", "自动关机小软件运行中...", ToolTipIcon.Info);
+            }
+
+            Shortcut sc = new Shortcut();
+            //  sc.Path = "目标文件地址";
+            // sc.Arguments = "启动参数";
+            // sc.WorkingDirectory = "启动文件的文件夹";
+            //sc.Description = "描述";
+            //sc.Save("这个会计方式保存在哪");
+            sc.Path = System.Environment.CurrentDirectory + @"\aotu_poweroff.exe";
+            sc.Arguments = "s";
+            sc.WorkingDirectory = System.Environment.CurrentDirectory;
+            sc.Description = "aotu_poweroff";
+            sc.Save(@"C:\Users\" + System.Environment.MachineName.ToLower() + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\aotu_poweroff.lnk");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             set_hour = Convert.ToInt32(tb1.Text);
+            set_min = Convert.ToInt32(tb2.Text);
+            lb2.Text="时间已设定为："+set_hour+"时"+set_min+"分";
+        }
+
+        private void dclick(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;  //托盘图标隐藏
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+            this.ShowInTaskbar = true;
+        }
+
+        private void Form1_size_changed(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)  //判断是否最小化
+            {
+
+                this.ShowInTaskbar = false;  //不显示在系统任务栏
+                notifyIcon1.Visible = true;  //托盘图标可见
+                this.notifyIcon1.ShowBalloonTip(500, "提示", "自动关机小软件运行中...", ToolTipIcon.Info);
+            }
+        }
+
+        private void chk_changed(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                Shortcut sc = new Shortcut();
+                //  sc.Path = "目标文件地址";
+                // sc.Arguments = "启动参数";
+                // sc.WorkingDirectory = "启动文件的文件夹";
+                //sc.Description = "描述";
+                //sc.Save("这个会计方式保存在哪");
+                sc.Path = System.Environment.CurrentDirectory + @"\aotu_poweroff.exe";
+                sc.Arguments = "s";
+                sc.WorkingDirectory = System.Environment.CurrentDirectory;
+                sc.Description = "aotu_poweroff";
+                sc.Save(@"C:\Users\" + System.Environment.MachineName.ToLower() + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\aotu_poweroff.lnk");
+            }
+            else {
+                FileInfo file2 = new FileInfo(@"C:\Users\" + System.Environment.MachineName.ToLower() + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\aotu_poweroff.lnk");
+                if (file2.Exists)
+                {
+                    file2.Delete(); //删除单个文件
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(asm.Location);
+            //   string versionStr = String.Format("{0}.{1}", fvi.ProductMajorPart, fvi.ProductMinorPart);
+            string versionStr = String.Format("{0}", fvi.FileVersion);
+
+            MessageBox.Show("版本号：" + versionStr + " ╮(╯﹏╰）╭\r\n获取最新程序：http://cc00559.csec.corp/", "关于");
+        }
+    }
+}
